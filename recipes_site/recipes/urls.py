0674f202cc9fb88bff994990bd42c655c8340a3e
Ivan Khabarov <ivan@ihabarov.ru>
2024-12-02 23:01:02 +0300

    Создан Сайт

---
+++ recipes_site/recipes/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+from django.urls import path
+from django.contrib import admin
+from django.views.generic import TemplateView
+#recipes_site/recipes/views.py
+from django.urls import path, include, reverse_lazy, reverse, resolve
+
+from recipes_site import settings
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('user/', views.user, name='user'),
+    path('logout/', views.user_logout, name='logout'),
+    path('cooker/', views.cooker, name='cooker'),
+    path('recipe_add/', views.recipe_add, name='recipe_add'),
+    path('recipe_edit/<int:recipe_id>', views.recipe_edit, name='recipe_edit'),
+    path('recipe_detail/<int:recipe_id>', views.recipe_detail, name='recipe_detail'),
+    path('recipe_delete/<int:recipe_id>', views.recipe_delete, name='recipe_delete'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = views.handler404
+
+handler500 = views.handler500
